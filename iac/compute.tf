c1778194e2aad2acf7d62b9dbc2c329b9ecaed06
nessim-ben-ammar <nessim.benammar@gmail.com>
2025-07-12 07:50:32 +0000

    working baseline

--- iac/compute.tf
+++ iac/compute.tf
@@ -1,11 +1,16 @@
 # Get the latest Ubuntu 24.04 AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"]
+  owners      = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-noble-24.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
 
@@ -28,17 +33,17 @@ resource "local_file" "ssh_public_key" {
   filename = "${path.module}/ssh_keys/aws-instance-ssh-key.pub"
 }
 
-resource "aws_key_pair" "cloud_vpn_key" {
-  key_name   = "cloud-vpn-key"
+resource "aws_key_pair" "cloud_vpn_ssh_key" {
+  key_name   = "cloud-vpn-ssh-key"
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 
 resource "aws_instance" "cloud_vpn_instance" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t3.medium"
-  subnet_id              = aws_subnet.cloud_vpn_pub_sn.id
-  vpc_security_group_ids = [aws_security_group.cloud_vpn_sg.id]
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.cloud_vpn_pub_sn.id
+  vpc_security_group_ids      = [aws_security_group.cloud_vpn_sg.id]
   associate_public_ip_address = true
-  key_name               = aws_key_pair.cloud_vpn_key.key_name
-  user_data              = file("${path.module}/user-data.yaml")
+  key_name                    = aws_key_pair.cloud_vpn_ssh_key.key_name
+  user_data                   = file("${path.module}/user-data.yaml")
 }
